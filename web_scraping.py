4536bca7d63cb3e8bab042fb6c5f359aad5bd7af
avkav <avkavrecic@gmail.com>
2024-07-23 12:01:34 +0200

    Web Scrapping funcionando

--- web_scraping.py
+++ web_scraping.py
@@ -4,39 +4,46 @@ from bs4 import BeautifulSoup
 import csv
 
 #Variable con la dirección del sitio a hacer scraping
-url = "https://quotes.toscrape.com/ "
+url = "https://quotes.toscrape.com/"
 
-#Realizar la petición con request y guardamos el contenido de la pagina
-frases_to_scrape = requests.get(url)
+try:
+    # Realizar la petición con request y guardar el contenido de la pagina
+    frases_to_scrape = requests.get(url)
+    frases_to_scrape.raise_for_status()  # Para manejar errores HTTP
+except requests.exceptions.RequestException as e:
+    print(f"Error al realizar la petición: {e}")
+    exit()
 
 #Parsear la información, mediante una instancia de beautiful soup
 soup = BeautifulSoup(frases_to_scrape.text, 'lxml')
 #Confirmar el tipo de dato de la "sopa" creada
-type(soup)
+print(type(soup))
 
 #Imprimir con formato
 print(soup.prettify())
 
 #Apuntar a los artículos para extraerlos
-frases = soup.find_all('div', attrs={'class':'quote'})
-frases
+frases_html = soup.find_all('div', attrs={'class':'quote'})
+
 
 # Lista temporal para guardar los frases
 frases = []
 
 #iterar los frases y obtener las variables
-for frase in frases:
-    contenido_frase = frase.find('span', attrs={'class':'text'}).get_text()[1:]
-    # contenido_frase = frase.span.get('text')
-    # about_frase = frase.img.get('src')
-    # precio_frase = precio_frase_raw[1:]
-    # divisa = precio_frase_raw[0]
-    temp_row = [contenido_frase]
+for frase in frases_html:
+    contenido_frase = frase.find('span', attrs={'class': 'text'}).get_text()
+    autor_frase = frase.find('small', attrs={'class': 'author'}).get_text()
+    tags_frase = [tag.get_text() for tag in frase.find_all('a', attrs={'class': 'tag'})]
+    
+    temp_row = [contenido_frase, autor_frase, tags_frase]
     frases.append(temp_row)
     print(temp_row)
-frases_df = pd.DataFrame(frases,columns=['contenido_frase'])
+
+# Crear un DataFrame de pandas con las frases
+frases_df = pd.DataFrame(frases, columns=['Contenido', 'Autor', 'Tags'])
+
 
 
 # Inspeccionar el dataframe
-frases_df.head(10)    
+print(frases_df.head(10))    
 
